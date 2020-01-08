5b098112e4bebe13c7fec698f33794f38365bd03
DonkeyRit <alekseevda24@gmail.com>
2020-01-08 18:57:29 +0400

    [Refactoring] Add Data Acess Layer Abstractions

---
+++ TaskManagement/DAL.Abstractions/IRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Abstractions
+{
+    public class IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public IRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
+            )
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+        
+        public virtual TEntity GetByID(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (_context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ TaskManagement/DAL.Abstractions/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Type = Models.Type;
+
+namespace DAL.Abstractions
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly DbContext _context;
+        private IRepository<AssignedTask> _assignedTaskRepository;
+        private IRepository<Complexity> _complexityRepository;
+        private IRepository<Employee> _employeeRepository;
+        private IRepository<EventLog> _eventLogRepository;
+        private IRepository<Qualification> _qualificationRepository;
+        private IRepository<Result> _resultRepository;
+        private IRepository<Status> _statusRepository;
+        private IRepository<Task> _taskRepository;
+        private IRepository<Type> _typeRepository;
+        private bool _disposed;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<AssignedTask> AssignedTaskRepository => _assignedTaskRepository ?? (_assignedTaskRepository = new IRepository<AssignedTask>(_context));
+
+        public IRepository<Complexity> ComplexityRepository => _complexityRepository ?? (_complexityRepository = new IRepository<Complexity>(_context));
+
+        public IRepository<Employee> EmployeeRepository => _employeeRepository ?? (_employeeRepository = new IRepository<Employee>(_context));
+
+        public IRepository<EventLog> EventLogRepository => _eventLogRepository ?? (_eventLogRepository = new IRepository<EventLog>(_context));
+
+        public IRepository<Qualification> QualificationRepository => _qualificationRepository ?? (_qualificationRepository = new IRepository<Qualification>(_context));
+
+        public IRepository<Result> ResultRepository => _resultRepository ?? (_resultRepository = new IRepository<Result>(_context));
+
+        public IRepository<Status> StatusRepository => _statusRepository ?? (_statusRepository = new IRepository<Status>(_context));
+
+        public IRepository<Task> TaskRepository => _taskRepository ?? (_taskRepository = new IRepository<Task>(_context));
+
+        public IRepository<Type> TypeRepository => _typeRepository ?? (_typeRepository = new IRepository<Type>(_context));
+        
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this._disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
